241618c71cce9e0b113c7b41a4729f552463ae25
Suren220695 <surendaranpalani@gmail.com>
2025-08-02 21:54:21 +0530

    Delete jobSupportJavascriptPlaywright directory

--- jobSupportJavascriptPlaywright/cucumber.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    default: {
-        require: ['features/step-definitions/*.js', 'features/support/*.js'],
-        format: ['progress-bar', 'html:cucumber-report.html'],
-        formatOptions: { snippetInterface: 'async-await' }
-    }
-}; 
